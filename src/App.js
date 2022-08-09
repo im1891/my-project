422db539f35f7e812537aaae32d945d6ba623441
im1891 <im1891@gmail.com>
2022-08-09 12:14:01 +0300

    Накидал примерную структуру сайта с помощью Grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://cdn.logo.com/hotlink-ok/logo-social.png"/>
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                        <div>Post 3</div>
+                        <div>Post 4</div>
+                        <div>Post 5</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
