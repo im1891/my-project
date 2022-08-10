f644774aacfed436abb1dfad670912c4ff444faa
im1891 <im1891@gmail.com>
2022-08-10 17:31:25 +0300

    Накидал примерную структуру страницы с диалогами

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-
+import classes from './Dialogs.module.css';
 
 function Dialogs(props) {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className='dialog'>
+                    Anya
+                </div>
+                <div className='dialog'>
+                    Pasha
+                </div>
+                <div className='dialog'>
+                    Dima
+                </div>
+                <div className='dialog'>
+                    Viktor
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className="message">Hello</div>
+                <div className="message">How are you?</div>
+                <div className="message">What are you doing?</div>
+            </div>
         </div>
     )
 
